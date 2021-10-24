090380eebad17219f91beda03d1d44a4ceb6d06b
galiv <haliv.andriy99@gmail.com>
2021-10-24 12:27:58 +0300

    added entities

---
+++ ChatMeServer/ChatMeAPI/DataAccessLayer/Entities/ConversationInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Entities
+{
+    public class ConversationInfo
+    {
+        public int Id { get; set; }
+
+        public int AdminId { get; set; }
+        public User Admin { get; set; }
+
+        public string GroupName { get; set; }
+
+        public string PhotoName { get; set; }
+
+        public int ConversationId { get; set; }
+        public Conversation Conversation { get; set; }
+    }
+}
---
+++ ChatMeServer/ChatMeAPI/DataAccessLayer/Entities/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Entities
+{
+    public class Message
+    {
+        public int Id { get; private set; }
+
+        public string Content { get; set; }
+
+        public string photo { get; set; }
+
+        public DateTime? TimeCreated { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public int? ChatId { get; set; }
+        public Conversation Chat { get; set; }
+    }
+}
