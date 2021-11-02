df5778e88d8ef22daa56b7990a7c2dc6e5cf7a3c
Mykhailo Sorochynskyi <mishash20161506@gmail.com>
2021-11-02 19:00:10 +0200

    dev:added addphotodto and icache

---
+++ ChatMeServer/ChatMeAPI/BusinessLogicLayer/Models/PhotoDto/Requests/AddPhotoDto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace BusinessLogicLayer.Models.PhotoDto.Requests
+{
+    public class AddPhotoDto
+    {
+        public int UserId { get; set; }
+
+        [Required]
+        public int ConversationId { get; set; }
+
+        [Required]
+        public IFormFile UploadedFile { get; set; }
+    }
+}
