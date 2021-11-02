79351b99110a5661a723849fce1f53b868b7456c
kushnirolesya <kushnirolesya2000@gmail.com>
2021-11-02 19:09:06 +0200

    added authModels

---
+++ ChatMeServer/ChatMeAPI/BusinessLogicLayer/Models/AuthModels/RegisterModel.cs
@@ -0,0 +1,36 @@
+﻿using DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BusinessLogicLayer.Models.AuthModels
+{
+    public class RegisterModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(5)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string PasswordConfirm { get; set; }
+
+        [Required]
+        public string NickName { get; set; }
+
+        [Required]
+        public Sex Sex { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        [Range(6, 100)]
+        public int Age { get; set; }
+    }
+}
