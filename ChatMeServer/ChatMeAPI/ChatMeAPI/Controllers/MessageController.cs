2ca9940f4c1a5c53bb1ed5341ca8c010371a30ed
kushnirolesya <kushnirolesya2000@gmail.com>
2021-11-07 18:20:30 +0200

    added message and user controllers

---
+++ ChatMeServer/ChatMeAPI/ChatMeAPI/Controllers/MessageController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatMeAPI.Controllers
+{
+    public class MessageController
+    {
+    }
+}
