2f16070dd31137f7d89ca998056ff98e3c2faaf8
Mykhailo Sorochynskyi <mishash20161506@gmail.com>
2021-11-07 18:13:04 +0200

    dev: added auth and channel controllers

---
+++ ChatMeServer/ChatMeAPI/ChatMeAPI/Controllers/ChannelController.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+using Application.Models.ChatDto.Requests;
+using Application.Models.ConversationDto.Requests;
+using BusinessLogicLayer.IServices;
+using InfrastructureLayer.Extensions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MessengerAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class ChannelController : ControllerBase
+    {
+        private readonly IGroupService _groupService;
+        public ChannelController(IGroupService groupService)
+        {
+            _groupService = groupService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SubscribeForChannel([FromBody] AddConversationRequest request)
+        {
+            request.userId = HttpContext.GetUserId();
+
+            await _groupService.SubscribeAsync(request);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateChannel([FromBody] AddGroupRequest request)
+        {
+            request.UserId = HttpContext.GetUserId();
+
+            await _groupService.CreateGroupAsync(request);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LeaveChannel([FromBody] LeaveGroupRequest request)
+        {
+            request.UserId = HttpContext.GetUserId();
+
+            await this._groupService.LeaveGroupAsync(request);
+
+            return Ok();
+        }
+    }
+}
