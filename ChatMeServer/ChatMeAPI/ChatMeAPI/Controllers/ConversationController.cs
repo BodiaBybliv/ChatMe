1dd234c996dc2e89701200c690bdbb3bc882507f
Bohdan <179739@gmail.com>
2021-11-07 18:17:53 +0200

    added conversation and group controllers

---
+++ ChatMeServer/ChatMeAPI/ChatMeAPI/Controllers/ConversationController.cs
@@ -0,0 +1,85 @@
+﻿using Application.Models.ChatDto.Requests;
+using Application.Models.ChatDto.Responces;
+using Application.Models.ConversationDto.Requests;
+using Application.Models.ConversationDto.Responces;
+using BusinessLogicLayer.IServices;
+using BusinessLogicLayer.Models.PhotoDto.Requests;
+using BusinessLogicLayer.Models.UserDto.Requests;
+using InfrastructureLayer.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatMeAPI.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("api/[controller]/[action]")]
+    public class ConversationController : ControllerBase
+    {
+        private readonly IConversationService _conversationService;
+
+        public ConversationController(IConversationService conversationService)
+        {
+            _conversationService = conversationService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateChat([FromBody] AddConversationRequest request)
+        {
+            request.userId = HttpContext.GetUserId();
+
+            await _conversationService.CreateChatAsync(request);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<List<GetConversationDto>> GetConversations([FromQuery] GetChatsRequestDto request)
+        {
+            request.UserName = User.Identity.Name;
+
+            return await _conversationService.GetConversationsAsync(request);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangeConversationPhoto(IFormCollection collection, [FromQuery(Name = "chatId")] int chatId)
+        {
+            if (collection.Files[0] != null)
+            {
+                await _conversationService.ChangePhotoAsync(new AddPhotoDto()
+                {
+                    ConversationId = chatId,
+                    UserId = HttpContext.GetUserId(),
+                    UploadedFile = collection.Files[0]
+                });
+
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+        [HttpGet]
+        public async Task<List<SearchConversationResponce>> Search([FromQuery] SearchRequest request)
+        {
+            request.UserId = HttpContext.GetUserId();
+
+            return await this._conversationService.SearchConversation(request);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromQuery] DeleteRequest request)
+        {
+            request.UserId = HttpContext.GetUserId();
+
+            await this._conversationService.DeleteConversationAsync(request);
+
+            return Ok();
+        }
+    }
+}
---
+++ ChatMeServer/ChatMeAPI/ChatMeAPI/Controllers/GroupController.cs
@@ -0,0 +1,53 @@
+﻿using Application.Models.ConversationDto.Requests;
+using BusinessLogicLayer.IServices;
+using InfrastructureLayer.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatMeAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class GroupController : ControllerBase
+    {
+        private IGroupService _conversationService;
+
+        public GroupController(IGroupService conversationService)
+        {
+            _conversationService = conversationService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddNewGroupMember([FromBody] AddConversationMemberRequest request)
+        {
+            request.UserId = HttpContext.GetUserId();
+
+            await this._conversationService.AddConversationMemberAsync(request);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LeaveGroup([FromBody] LeaveGroupRequest request)
+        {
+            request.UserId = HttpContext.GetUserId();
+
+            await this._conversationService.LeaveGroupAsync(request);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateGroup([FromBody] AddGroupRequest request)
+        {
+            request.UserId = HttpContext.GetUserId();
+
+            await _conversationService.CreateGroupAsync(request);
+
+            return Ok();
+        }
+    }
+}
