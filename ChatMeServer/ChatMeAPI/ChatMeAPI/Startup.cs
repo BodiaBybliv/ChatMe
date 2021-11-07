7b81081555c47255efe61426b8c6e21437f65468
galiv <haliv.andriy99@gmail.com>
2021-11-07 18:35:07 +0200

    Added StartUp file and Chat Hub

--- ChatMeServer/ChatMeAPI/ChatMeAPI/Startup.cs
+++ ChatMeServer/ChatMeAPI/ChatMeAPI/Startup.cs
@@ -1,15 +1,26 @@
+using BusinessLogicLayer.Models.AuthModels;
+using ChatMeAPI.Hubs;
+using DataAccessLayer;
+using InfrastructureLayer;
+using InfrastructureLayer.AppSecurity;
+using InfrastructureLayer.Cache;
+using InfrastructureLayer.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using AutoMapper;
 
 namespace ChatMeAPI
 {
@@ -22,29 +33,186 @@ namespace ChatMeAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<MessengerContext>(options =>
+              options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
+              builder => builder.MigrationsAssembly(typeof(Startup).GetTypeInfo().Assembly.GetName().Name)));
+
+            services.AddDbContext<SecurityContext>(options =>
+              options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
+              builder => builder.MigrationsAssembly(typeof(Startup).GetTypeInfo().Assembly.GetName().Name)));
+
+            services.AddIdentity<SecurityUser, IdentityRole<int>>()
+                    .AddEntityFrameworkStores<SecurityContext>()
+                    .AddDefaultTokenProviders();
+
+            services.Configure<TokenOption>(Configuration.GetSection("OptionsForToken"));
+
+            services.Configure<FbOptions>(Configuration.GetSection("FacebookOptions"));
+
+            services.Configure<EmailOptions>(Configuration.GetSection("EmailOptions"));
+
+            services.Configure<CacheOptions>(Configuration.GetSection("CacheOptions"));
+
+            var optionsForToken = Configuration.GetSection("OptionsForToken")
+                                .Get<TokenOption>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+                {
+                    options.RequireHttpsMetadata = false;
+
+                    options.SaveToken = true;
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = optionsForToken.Issuer,
+                        ValidAudience = optionsForToken.Audience,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        IssuerSigningKey = optionsForToken.GetSymmetricSecurityKey(),
+                        ValidateIssuerSigningKey = true,
+                        ClockSkew = TimeSpan.Zero
+                    };
+
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnMessageReceived = context =>
+                        {
+                            if (context.Request.Query.TryGetValue("token", out StringValues token))
+                            {
+                                context.Token = token;
+                            }
+
+                            return Task.CompletedTask;
+                        },
+                        OnAuthenticationFailed = context =>
+                        {
+                            var te = context.Exception;
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 5;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+            });
+
+            services.AddServices();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.WithOrigins("https://chatterangular.azurewebsites.net", "http://localhost:4200")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+
+            services.AddSignalR(configure =>
+            {
+                configure.ClientTimeoutInterval = TimeSpan.FromSeconds(200);
+            });
+
+            services.AddAutoMapper(typeof(MappingProfile));
+
+            services.AddMemoryCache();
+
+            //services.AddStackExchangeRedisCache(options =>
+            //{
+            //    options.Configuration = "localhost";
+            //    options.InstanceName = "RedisCache";
+            //});
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+              {
+                {
+                  new OpenApiSecurityScheme
+                  {
+                    Reference = new OpenApiReference
+                      {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                      },
+                      Scheme = "oauth2",
+                      Name = "Bearer",
+                      In = ParameterLocation.Header,
+
+                    },
+                    new List<string>()
+                  }
+                });
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, UserManager<SecurityUser> userManager,
+            RoleManager<IdentityRole<int>> roleManager, SecurityContext context, MessengerContext mescontext, IConfiguration config)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
+            app.UseStaticFiles();
+
+            DataInitializer.SeedData(userManager, roleManager, context, mescontext, config).Wait();
+
+            app.UseCors("CorsPolicy");
+
+            app.UseErrorHandling();
+
+            app.UseAuthentication();
+
             app.UseAuthorization();
+
+            app.UseIdHandler();
+
+            app.UseUserStatusMiddleware();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+
+                endpoints.MapHub<Chat>("/chat");
             });
         }
     }
